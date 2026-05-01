0845a6c47f81001208cebce3949283976de39e15
Abenezer Anglo <111607144+AbaSheger@users.noreply.github.com>
2026-05-01 04:32:15 +0200

    Format auto configuration and add enabled condition

--- guardrail4j-spring-boot-starter/src/main/java/io/github/abasheger/guardrail4j/config/Guardrail4jAutoConfiguration.java
+++ guardrail4j-spring-boot-starter/src/main/java/io/github/abasheger/guardrail4j/config/Guardrail4jAutoConfiguration.java
@@ -8,16 +8,49 @@ import io.github.abasheger.guardrail4j.store.InMemoryUsageStore;
 import io.github.abasheger.guardrail4j.store.UsageStore;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 @EnableConfigurationProperties(Guardrail4jProperties.class)
+@ConditionalOnProperty(prefix = "guardrail4j", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class Guardrail4jAutoConfiguration {
-    @Bean @ConditionalOnMissingBean public UsageStore usageStore() { return new InMemoryUsageStore(); }
-    @Bean @ConditionalOnMissingBean public CostEstimator costEstimator(Guardrail4jProperties p) { return new CostEstimator(p); }
-    @Bean @ConditionalOnMissingBean public GuardrailDecisionEngine decisionEngine(UsageStore s, Guardrail4jProperties p) { return new GuardrailDecisionEngine(s, p); }
-    @Bean @ConditionalOnMissingBean public GuardrailInterceptor interceptor(UsageStore s, CostEstimator c, GuardrailDecisionEngine d, Guardrail4jProperties p) { return new GuardrailInterceptor(s, c, d, p); }
-    @Bean @ConditionalOnWebApplication public GuardrailController guardrailController(UsageStore s, Guardrail4jProperties p) { return new GuardrailController(s, p); }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public UsageStore usageStore() {
+        return new InMemoryUsageStore();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CostEstimator costEstimator(Guardrail4jProperties properties) {
+        return new CostEstimator(properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public GuardrailDecisionEngine decisionEngine(UsageStore usageStore, Guardrail4jProperties properties) {
+        return new GuardrailDecisionEngine(usageStore, properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public GuardrailInterceptor interceptor(
+            UsageStore usageStore,
+            CostEstimator costEstimator,
+            GuardrailDecisionEngine decisionEngine,
+            Guardrail4jProperties properties
+    ) {
+        return new GuardrailInterceptor(usageStore, costEstimator, decisionEngine, properties);
+    }
+
+    @Bean
+    @ConditionalOnWebApplication
+    @ConditionalOnMissingBean
+    public GuardrailController guardrailController(UsageStore usageStore, Guardrail4jProperties properties) {
+        return new GuardrailController(usageStore, properties);
+    }
 }
